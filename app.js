1caa5ad545ed7dc6d1580ac0731586cff8ecf194
deppravacion <deppravacion@gmail.com>
2022-09-18 16:40:20 -0600

    add app refactor

--- app.js
+++ app.js
@@ -1,22 +1,60 @@
-const clickOrder = [];
+const clickOrderArry = [];
 const numTiles = document.querySelectorAll(".tile").length;
 
-
-document.addEventListener('click', event => {
+function onlyTiles(event) {
   if (!event.target.classList.contains('tile')) return;
-  if(clickOrder.includes(event.target)) return;
-  console.log(event.target);
-  event.target.classList.add('selected');
-  clickOrder.push(event.target);
+}
 
-  if (clickOrder.length >= numTiles) {
-    const interval = setInterval(() => {
-      const tile = clickOrder.shift();
-      tile.classList.remove('selected');
-      if (clickOrder.length === 0) {
-        clearInterval(interval);
-      }
-    }, 600)
+function noDubs(event) {
+  if(clickOrderArry.includes(event.target)) return;
+}
+
+function addClass(event, nam) {
+  event.target.classList.add(nam);
+}
+
+function addToArry(event) {
+  clickOrderArry.push(event.target);
+}
+
+function shiftArry() {
+  const tile = clickOrderArry.shift();
+  tile.classList.remove('selected');
+}
+
+function intervalFinished() {
+  if (clickArryEmpty) {
+    clearInterval(interval);
   }
-})
+}
+
+function myInterval() {
+  const interval = setInterval(() => {
+    shiftArry();
+    intervalFinished();
+  }, 600)
+}
+
+function fillTiles(event) {
+  onlyTiles(event);
+  noDubs(event);
+  addClass(event, 'selected');
+  addToArry(event);
+}
+
+function emptyTiles() {
+  let allBoxesClicked = clickOrderArry.length >= numTiles;
+  let clickArryEmpty = clickOrderArry.length === 0;
+  
+  if (allBoxesClicked) { myInterval(); }
+}
+
+function startSetUp() {
+  document.addEventListener('click', event => {
+    fillTiles(event);
+    emptyTiles();
+  })
+}
+
+startSetUp();
 
