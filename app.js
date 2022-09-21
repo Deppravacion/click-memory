377ea00553bb28b475cd42baf2288522067d4f26
deppravacion <deppravacion@gmail.com>
2022-09-21 15:00:20 -0600

    add refactor and original set

--- app.js
+++ app.js
@@ -1,60 +1,40 @@
-const clickOrderArry = [];
+const clickOrder = []; 
 const numTiles = document.querySelectorAll(".tile").length;
 
-function onlyTiles(event) {
-  if (!event.target.classList.contains('tile')) return;
+function addClass(elm, prop) {
+  elm.target.classList.add(prop);
 }
 
-function noDubs(event) {
-  if(clickOrderArry.includes(event.target)) return;
+function pushFunc(elm) {
+  clickOrder.push(elm.target);
 }
 
-function addClass(event, nam) {
-  event.target.classList.add(nam);
+function shiftFunc(prop) {
+  clickOrder.shift().classList.remove(prop);
 }
 
-function addToArry(event) {
-  clickOrderArry.push(event.target);
-}
-
-function shiftArry() {
-  const tile = clickOrderArry.shift();
-  tile.classList.remove('selected');
-}
-
-function intervalFinished() {
-  if (clickArryEmpty) {
-    clearInterval(interval);
+function siphonFunc() {
+  let clickTotal = clickOrder.length;
+ 
+  if (clickTotal >= numTiles) {
+    const interval = setInterval(() => {
+      shiftFunc('selected');
+      if (clickOrder.length === 0) {
+        clearInterval(interval);
+      }
+    }, 600)
   }
 }
 
-function myInterval() {
-  const interval = setInterval(() => {
-    shiftArry();
-    intervalFinished();
-  }, 600)
-}
-
-function fillTiles(event) {
-  onlyTiles(event);
-  noDubs(event);
-  addClass(event, 'selected');
-  addToArry(event);
-}
-
-function emptyTiles() {
-  let allBoxesClicked = clickOrderArry.length >= numTiles;
-  let clickArryEmpty = clickOrderArry.length === 0;
-  
-  if (allBoxesClicked) { myInterval(); }
-}
-
-function startSetUp() {
+function eventFunc() {
   document.addEventListener('click', event => {
-    fillTiles(event);
-    emptyTiles();
+    if (!event.target.classList.contains('tile')) return;
+    if(clickOrder.includes(event.target)) return;
+  
+    addClass(event, 'selected');
+    pushFunc(event);
+    siphonFunc();
   })
 }
 
-startSetUp();
-
+eventFunc();
